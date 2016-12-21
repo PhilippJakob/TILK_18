7ed185e13d6a97af0fb592e95a7151fdfd227782
hannapel.mareike <hannapel.mareike@PC230101.osz.intern>
2016-12-21 12:36:32 +0100

    jihiuhuh

--- Einstiegsübung in Git/src/gitEinstieg/EineKlasseFürAlle.java
+++ Einstiegsübung in Git/src/gitEinstieg/EineKlasseFürAlle.java
@@ -1,6 +1,6 @@
 /**
  * Angelegt 19.12.2016 Hr Jakob
- *
+ * geaendert 21.12.2016 12:30 Mareike Hannappel
  * Zweck: Jeder soll hier eine eigene Methode hinzufügen.
  * Nach dem Muster: public void durchwühlenMichael(){}
  *
@@ -10,9 +10,14 @@ package gitEinstieg;
 public class EineKlasseFürAlle
 {
   public void aufstehenHrKock(){}
+  //jhjukh
 
   public void streiten()
   {
 	// Kock
+  }
+  public void arbeitenMareike()
+  {
+
   }
 }
